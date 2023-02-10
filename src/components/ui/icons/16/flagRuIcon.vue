ec6ac0dc55e065cd68d4f0ef2cb4651f526e57fc
lifiij <migoviprog@gmail.com>
2023-02-10 17:21:46 +0400

    added icons

---
+++ src/components/ui/icons/16/flagRuIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    width="22"
+    height="15"
+    viewBox="0 0 22 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      width="20"
+      height="4"
+      transform="matrix(-1 0 0 1 21 1)"
+      fill="white"
+    />
+    <rect
+      x="0.05"
+      y="-0.05"
+      width="20.1"
+      height="4.1"
+      transform="matrix(-1 0 0 1 21.1 1)"
+      stroke="black"
+      stroke-opacity="0.5"
+      stroke-width="0.1"
+    />
+    <rect
+      width="20"
+      height="5"
+      transform="matrix(-1 0 0 1 21 5)"
+      fill="#0000FF"
+    />
+    <rect
+      x="0.05"
+      y="-0.05"
+      width="20.1"
+      height="5.1"
+      transform="matrix(-1 0 0 1 21.1 5)"
+      stroke="black"
+      stroke-opacity="0.5"
+      stroke-width="0.1"
+    />
+    <rect
+      width="20"
+      height="4"
+      transform="matrix(-1 0 0 1 21 10)"
+      fill="#FF0000"
+    />
+    <rect
+      x="0.05"
+      y="-0.05"
+      width="20.1"
+      height="4.1"
+      transform="matrix(-1 0 0 1 21.1 10)"
+      stroke="black"
+      stroke-opacity="0.5"
+      stroke-width="0.1"
+    />
+  </svg>
+</template>
